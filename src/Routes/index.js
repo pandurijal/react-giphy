c53cbaf59a466cced3997f2379a414aa7d7e3ef3
pandurijal <pandurijal96@gmail.com>
2019-11-24 06:21:12 +0700

    responsive

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,8 @@
 import MainPage from '../Pages/MainPage';
-import FavPage from '../Pages/FavPage';
 
 export const routes = [
   {
     path: '/',
-    component: MainPage,
-    exact: true
-  },
-  {
-    path: '/search',
     component: MainPage,
     exact: true
   },
