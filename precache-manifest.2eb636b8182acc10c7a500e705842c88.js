5fac4d33d46f03bf92484c539237d8e750222b1d
pandurijal <pandurijal96@gmail.com>
2019-11-24 07:02:38 +0700

    Updates

---
+++ precache-manifest.2eb636b8182acc10c7a500e705842c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f104a13ce38b1039f5cdc970a6051f26",
+    "url": "/gallereasy/index.html"
+  },
+  {
+    "revision": "0a5a95f7a504483b7d30",
+    "url": "/gallereasy/static/css/main.8a02d50e.chunk.css"
+  },
+  {
+    "revision": "479c0f4c5fcc2122f371",
+    "url": "/gallereasy/static/js/2.4dcf762b.chunk.js"
+  },
+  {
+    "revision": "0a5a95f7a504483b7d30",
+    "url": "/gallereasy/static/js/main.1d7de4ef.chunk.js"
+  },
+  {
+    "revision": "163d005f81a8e3152445",
+    "url": "/gallereasy/static/js/runtime-main.0a8bdcdf.js"
+  }
+]);
