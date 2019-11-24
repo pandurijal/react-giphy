f268b544689da0f95c32ec010d549b354a89420b
pandurijal <pandurijal96@gmail.com>
2019-11-24 09:21:12 +0700

    title

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Footer } from '../Components';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Footer } from "../Components";
 
-import SearchPage from './SearchPage';
-import FavPage from './FavPage';
+import SearchPage from "./SearchPage";
+import FavPage from "./FavPage";
 
 class App extends Component {
   constructor(props) {
@@ -12,39 +12,39 @@ class App extends Component {
   }
 
   onFavClicked = (id, srcImg) => {
-    let favImg = JSON.parse(localStorage.getItem('favImg'));
+    let favImg = JSON.parse(localStorage.getItem("favImg"));
     const favIndex = favImg.findIndex(val => val.id === id);
     if (favIndex !== -1) {
       favImg = favImg.filter(val => val.id !== id);
     } else {
       favImg.push({ id, srcImg });
     }
-    localStorage.setItem('favImg', JSON.stringify(favImg));
+    localStorage.setItem("favImg", JSON.stringify(favImg));
   };
 
   render() {
-    const favImg = JSON.parse(localStorage.getItem('favImg'));
+    const favImg = JSON.parse(localStorage.getItem("favImg"));
     const { pathname } = this.props.location;
 
     return (
       <div className="App">
         <Navbar title="GallerEasy">
           <Link to="/">
-            <p className={`app-menu ${pathname === '/' ? 'active' : ''}`}>
+            <p className={`app-menu ${pathname === "/" ? "active" : ""}`}>
               Search
             </p>
           </Link>
           <Link to="/fav">
-            <p className={`app-menu ${pathname === '/fav' ? 'active' : ''}`}>
-              Favourites {`${favImg.length ? favImg.length : ''}`}
+            <p className={`app-menu ${pathname === "/fav" ? "active" : ""}`}>
+              Favourites {`${favImg.length ? `(${favImg.length})` : ""}`}
             </p>
           </Link>
         </Navbar>
         <div className="container">
-          {pathname === '/' && (
+          {pathname === "/" && (
             <SearchPage favImg={favImg} onFavClicked={this.onFavClicked} />
           )}
-          {pathname === '/fav' && (
+          {pathname === "/fav" && (
             <FavPage favImg={favImg} onFavClicked={this.onFavClicked} />
           )}
         </div>
